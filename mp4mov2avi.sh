cad29b7f7bd9942f2fd98568a4c157fa1193c787
cutsea110 <cutsea110@gmail.com>
2015-09-09 06:48:22 +0900

    update for GHC-7.10.2 and use stack

--- mp4mov2avi.sh
+++ mp4mov2avi.sh
@@ -1,6 +1,6 @@
 pushd $1;
 
-for f in `ls *.MP4 *.MOV`; do
+for f in `ls *.MP4 *.MOV *.mp4 *.mov`; do
   echo ffmpeg -y -i ${f} -vcodec mjpeg -acodec libmp3lame -s hd720 ${f%.*}.avi;
   ffmpeg -y -i ${f} -vcodec mjpeg -acodec libmp3lame -s hd720 ${f%.*}.avi;
 done
